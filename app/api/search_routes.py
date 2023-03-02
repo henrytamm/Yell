03a0e0d96e6ed8f325a663c4f2a0e3dd193f83c5
Henry <henrytam95@gmail.com>
2023-03-01 18:57:47 -0800

    search

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
-from app.models import Biz, Review, Category, business_categories, db
+from app.models import Biz, Review, Category, business_categories, db, Hour
+from datetime import date, datetime, time
 
 search_routes = Blueprint('categories', __name__)
 
@@ -10,8 +11,6 @@ def bizes_by_category(categoryId):
     """
     Query for all bizes of the specified category by id
     """
-    print("in search route")
-
     category = Category.query.filter(Category.id == categoryId).first()
     bizes = category.biz
     newDict = {}
@@ -32,3 +31,40 @@ def bizes_by_category(categoryId):
         index += 1
 
     return newDict
+
+@search_routes.route('/open')
+def bizes_by_hours():
+    """
+    Query for all bizes by hours
+    """
+    today = date.weekday(date.today())
+    if today == 0:
+        day = "monday"
+    elif today == 1:
+        day = "tuesday"
+    elif today == 2:
+        day = "wednesday"
+    elif today == 3:
+        day = "thursday"
+    elif today == 4:
+        day = "friday"
+    elif today == 5:
+        day = "saturday"
+    elif today == 6:
+        day = "sunday"
+    # print(today)
+    now = datetime.now().time()
+    print(now)
+    print(type(now))
+    # current_time = now.strftime("%H:%M:%S")
+    # print(current_time)
+    print(type(time(10, 00)))
+    # print(type(time(int(current_time))))
+    # print(type(time(10,00)))
+    hours = Hour.query.filter(Hour.wednesday_open <= now and now <= Hour.wednesday_close)
+    # users = db.session.query(User).filter(User.birthday.between(start_range, end_range)).all()
+    # print(Hour.wednesday_open)
+    # hours = db.session.query(Hour).filter(now.between(Hour.wednesday_open, Hour.wednesday_close)).all()
+    print(hours)
+    return "d"
+ 
--- routes.md
+++ routes.md
@@ -4,20 +4,20 @@ Search / filters
 Reviews / ratings
 Map
 
-Users:
+<!-- Users:
 get current user ('/user')
 get user by id ('/user/<int:id>')
 edit user ('/user/<int:id>')
 logout user ('/user/<int:id>')
 login user ('/session')
-signup user ('/session)
+signup user ('/session) -->
 
 
-Business:
+<!-- Business:
 get all businesses ('/biz')
 create a business ('/biz')
 edit a business ('/biz/<int:bizId>')
-delete a business ('/biz/<int:bizId>')
+delete a business ('/biz/<int:bizId>') -->
 
 
 Search/Filters:
@@ -26,11 +26,12 @@ business owned by user ('/user/<int:id>/biz')
 businesses open ('/biz/open')
 
 
-Reviews:
+<!-- Reviews:
+get all reviews ('/reviews')
 get all reviews of business ('/biz/<int:bizId>/reviews')
 create a review ('/biz/<int:bizId>/reviews)
 edit a review ('/biz/<int:bizId>/reviews/<int:reviewId>)
-delete a review ('/biz/<int:bizId>/reviews/<int:reviewId>)
+delete a review ('/biz/<int:bizId>/reviews/<int:reviewId>) -->
 
 
 Map:
