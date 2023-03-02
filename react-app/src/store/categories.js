d4f4e615a1cdea29f92dde48e21c6cf582ea7838
Henry <henrytam95@gmail.com>
2023-03-01 21:59:14 -0800

    reducer files

--- react-app/src/store/categories.js
+++ react-app/src/store/categories.js
@@ -1,24 +1,24 @@
-// const GET_ALL_CATEGORY = "/categories/getAllCategory";
+const GET_ALL_CATEGORY = "/categories/getAllCategory";
 const GET_ONE_CATEGORY = "/categories/getOneCategory";
 
-// const getAllCategoryAction = (categories) => ({
-//   type: GET_ALL_CATEGORY,
-//   categories,
-// });
+const getAllCategoryAction = (categories) => ({
+  type: GET_ALL_CATEGORY,
+  categories,
+});
 
 const getOneCategoryAction = (category) => ({
   type: GET_ONE_CATEGORY,
   category,
 });
 
-// export const getAllCategory = () => async (dispatch) => {
-//   const res = await fetch(`/api/search/${categoryId}`);
-//   if (res.ok) {
-//     const category = await res.json();
-//     dispatch(getAllCategoryAction(category));
-//     return category;
-//   }
-// };
+export const getAllCategory = () => async (dispatch) => {
+  const res = await fetch(`/api/search/${categoryId}`);
+  if (res.ok) {
+    const category = await res.json();
+    dispatch(getAllCategoryAction(category));
+    return category;
+  }
+};
 
 export const getOneCategory = (categoryId) => async (dispatch) => {
   const res = await fetch(`/api/search/${categoryId}`);
@@ -35,12 +35,12 @@ export const bizReducer = (state = initialState, action) => {
   let newState = { ...state };
   switch (action.type) {
 
-    // case GET_ALL_CATEGORY: {
-    //     action.category.forEach((category) => {
-    //         newState[category.id] = category;
-    //       });
-    //       return newState;
-    //     }
+    case GET_ALL_CATEGORY: {
+        action.category.forEach((category) => {
+            newState[category.id] = category;
+          });
+          return newState;
+        }
 
     case GET_ONE_CATEGORY:
         newState[action.categoryId] = action.category
