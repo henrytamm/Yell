4b6ac50535f5f76989e06c113ac0439739108a58
Rachel <rachbohm@gmail.com>
2023-03-10 01:08:49 -0600

    changes

--- app/models/business_categories_jointable.py
+++ app/models/business_categories_jointable.py
@@ -1,9 +1,11 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-
-
+table_args = {}
+if environment == 'production':
+    table_args = {'schema': SCHEMA}
 
 business_categories = db.Table(
     "business_categories",
     db.Column("biz_id", db.Integer, db.ForeignKey(add_prefix_for_prod("bizes.id"), ondelete="CASCADE"), primary_key=True),
-    db.Column("category_id", db.Integer, db.ForeignKey(add_prefix_for_prod("categories.id"), ondelete="CASCADE"), primary_key=True)
+    db.Column("category_id", db.Integer, db.ForeignKey(add_prefix_for_prod("categories.id"), ondelete="CASCADE"), primary_key=True),
+    __table_args__ = table_args
 )
